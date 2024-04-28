ec6e1cf36668889e94abe3ff64035df10aa49ad9
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-04-28 12:01:23 -0400

    Create new screens and data dir functions

---
+++ src/import_screen.rs
@@ -0,0 +1,6 @@
+use dioxus::prelude::*;
+
+#[component]
+pub fn ImportScreen() -> Element {
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 #![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
+mod import_screen;
+mod index_screen;
 use dioxus::{desktop::Config, prelude::*};
+use index_screen::IndexScreen;
 /// # Sprout
 /// A stardew valley mod manager.
+
+#[derive(Routable, PartialEq, Clone)]
+enum Routes {
+    #[route("/")]
+    IndexScreen {},
+}
 
 fn main() {
     LaunchBuilder::desktop()
         .with_cfg(
             Config::new()
                 .with_background_color((39, 174, 96, 1))
-                .with_data_directory(dirs::data_dir().unwrap()) // set to $XDG_CONFIG_HOME/.svsprout
-                .with_resource_directory("./assets/")
+                .with_data_directory(get_data_dir()) // set to $XDG_CONFIG_HOME/.svsprout
+                // .with_resource_directory("./assets/")
                 .with_disable_context_menu(true), // disable right click menu
         )
         .launch(App);
@@ -17,5 +26,20 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    rsx!("hello, world!")
+    rsx! { Router::<Routes> {} }
+}
+
+pub fn get_data_dir() -> std::path::PathBuf {
+    let mut path = dirs::data_dir().unwrap_or_else(|| {
+        eprintln!("Unsupported operating system.");
+        std::process::exit(1);
+    });
+    path.push(".svsprout/");
+    if !path.exists() {
+        std::fs::create_dir(&path).unwrap_or_else(|_| {
+            eprintln!("No permission to write to data directory.");
+            std::process::exit(1);
+        });
+    };
+    path
 }
