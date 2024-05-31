037935de2c1c1161bbf4c5012ead69ab0893ff97
Raforawesome <ubuntuhasnoroblox@gmail.com>
2024-05-31 15:33:37 -0400

    Create completely custom titlebar

--- src/components/title_header.rs
+++ src/components/title_header.rs
@@ -1,10 +1,25 @@
 use dioxus::prelude::*;
+use dioxus::desktop::window;
 
 #[component]
 pub fn TitleHeader(sub_title: String) -> Element {
     rsx! {
+        div {
+            class: "title-buttons",
+            button {
+                class: "button title-button",
+                onclick: |_| window().set_minimized(true),
+                "–"
+            },
+            button {
+                class: "button title-button",
+                onclick: |_| window().close(),
+                "x"
+            }
+        }
         style { {include_str!("css/title_header.css")} }
         div {
+            onmousedown: |_| window().drag(),
             id: "title-header",
             class: "title-header",
             img { class: "icon", src: "/sprout@1x.png" },
