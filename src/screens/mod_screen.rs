5ab186b403d3502dee1fc522226cdf9f74aab89e
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-05-31 09:34:33 -0400

    Create custom table from scratch since HTML table bad

--- src/screens/mod_screen.rs
+++ src/screens/mod_screen.rs
@@ -1,10 +1,35 @@
-use crate::components::TitleHeader;
+use crate::{components::TitleHeader, AppState};
 use dioxus::prelude::*;
 use std::path::PathBuf;
 
 #[component]
-pub fn ModScreen(path: PathBuf) -> Element {
+pub fn ModScreen() -> Element {
+    let mut state: Signal<AppState> = use_context::<Signal<AppState>>();
+
     rsx! {
+        style { {include_str!("../css/mod_screen.css")} }
         TitleHeader { sub_title: "Mod List" }
+        div {
+            class: "mod-list",
+            div {
+                class: "header-row",
+                span { style: "margin-left:1.06rem", input { "type": "checkbox" } }
+                p { style: "margin-left:1.94rem", "Name" }
+                p { style: "margin-left:12.69rem", "Version" }
+                p { style: "margin-left:4.31rem", "Min. Game Version" }
+                p { style: "margin-left:4.31rem;margin-right:2rem", "Enabled" }
+            }
+        }
+        // table {
+        //     class: "mod-list",
+        //     tr {
+        //         class: "header-row",
+        //         th { style: "margin-left:1.06rem", input { "type": "checkbox" } }
+        //         th { style: "margin-left:1.94rem", "Name" }
+        //         th { style: "margin-left:12.69rem", "Version" }
+        //         th { style: "margin-left:4.31rem", "Min. Game Version" }
+        //         th { style: "margin-left:4.31rem;margin-right:2rem", "Enabled" }
+        //     }
+        // }
     }
 }
