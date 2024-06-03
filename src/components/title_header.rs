319c30ed634298e34a517b31650df84ae7b314d3
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-06-03 12:19:20 -0400

    Change custom window controls to icons

--- src/components/title_header.rs
+++ src/components/title_header.rs
@@ -1,28 +1,28 @@
-use dioxus::prelude::*;
 use dioxus::desktop::window;
+use dioxus::prelude::*;
 
 #[component]
 pub fn TitleHeader(sub_title: String) -> Element {
     rsx! {
+        style { {include_str!("css/title_header.css")} }
         div {
             class: "title-buttons",
             button {
-                class: "button title-button",
+                class: "material-symbols-outlined button title-button",
                 onclick: |_| window().set_minimized(true),
-                "–"
+                "close_fullscreen"
             },
             button {
-                class: "button title-button",
+                class: "material-symbols-outlined button title-button",
                 onclick: |_| window().close(),
-                "x"
+                "cancel"
             }
         }
-        style { {include_str!("css/title_header.css")} }
         div {
             onmousedown: |_| window().drag(),
             id: "title-header",
             class: "title-header",
-            img { class: "icon", src: "/sprout@1x.png" },
+            img { class: "sprout-icon", src: "/sprout@1x.png" },
             h1 { class: "title", "Sprout" },
             h1 { class: "subtitle", "|" },
             h1 { class: "subtitle", "{sub_title}" }
