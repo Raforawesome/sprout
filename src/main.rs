3aaddf3a52cb82a4b857efd8d6aa55afdf553970
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-06-03 12:25:44 -0400

    Move material icon stylesheet to CSS files

--- src/main.rs
+++ src/main.rs
@@ -43,7 +43,6 @@ fn App() -> Element {
 
     rsx! {
         style { {include_str!("../public/global.css")} }
-        link { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" }
         Router::<Routes> {}
     }
 }
