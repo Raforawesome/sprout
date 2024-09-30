a4c5f752f4a624c5ab031940ba518d5e7613dbae
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-09-30 16:19:50 -0400

    Completely redesign mod table to fix badly written alignment

--- src/screens/mod_screen.rs
+++ src/screens/mod_screen.rs
@@ -1,9 +1,52 @@
-use crate::{components::TitleHeader, AppState};
+use crate::{components::TitleHeader, interface::mod_types::Mod, AppState};
 use dioxus::prelude::*;
 
 #[component]
+pub fn ModRow(mod_obj: Mod, alt: bool) -> Element {
+    rsx! {
+        table {
+            class: if alt { "mod-row-alt" } else { "mod-row" },
+            tr {
+                style: "width:7%",
+                input {
+                    "type": "checkbox",
+                }
+            }
+            tr {
+                style: "width:43%",
+                p { {mod_obj.name()} }
+            }
+            tr {
+                style: "width:20%",
+                p { {mod_obj.version()} }
+            }
+            tr {
+                style: "width:20%",
+                p { {mod_obj.min_api_version()} }
+            }
+            tr {
+                style: "width:10%",
+                p { {mod_obj.enabled().to_string()} }
+            }
+        }
+    }
+}
+
+#[component]
 pub fn ModScreen() -> Element {
-    let mut state: Signal<AppState> = use_context::<Signal<AppState>>();
+    let state: Signal<AppState> = use_context::<Signal<AppState>>();
+    let mods: Vec<Mod> = crate::interface::mod_scanner::find_mods(state().game_path.as_path());
+
+    let mut alt: bool = true;
+    let mod_list = mods.iter().map(|m| {
+        alt = !alt;
+        rsx! {
+            ModRow {
+                mod_obj: m.clone(),
+                alt,
+            }
+        }
+    });
 
     rsx! {
         style { {include_str!("../css/mod_screen.css")} }
@@ -22,6 +65,7 @@ pub fn ModScreen() -> Element {
                         p { style: "margin-left:6.1%", "Min. API Version" }
                         p { style: "margin-left:6.1%", "Enabled" }
                     }
+                    {mod_list}
                 }
             }
             div {  // buttons container
