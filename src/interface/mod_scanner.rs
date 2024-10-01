ce8375703b1f8ce6e68934e8dfd94b790ea07291
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-10-01 13:44:14 -0400

    Substitute `json5` for `serde_json` to improve mod scanning
    Some manifest.json files technically weren't considered valid JSON due to trailing commas.

--- src/interface/mod_scanner.rs
+++ src/interface/mod_scanner.rs
@@ -2,13 +2,14 @@
 //! SMAPI mods and create Mod objects from them
 
 use super::mod_types::{Manifest, Mod};
+use json5;
 use std::path::{Path, PathBuf};
 
 /// # Scanner system
 /// This function scans the provided game directory
 /// for mod folders containing a manifest.json, then
 /// uses that information to create a [Vec] of [Mod] objects.
-pub fn find_mods(p: &Path) -> Vec<Mod> {
+pub fn find_active_mods(p: &Path) -> Vec<Mod> {
     #[cfg(not(target_os = "macos"))]
     let mod_dir: PathBuf = p.join("Mods/");
     #[cfg(target_os = "macos")]
@@ -23,11 +24,15 @@ pub fn find_mods(p: &Path) -> Vec<Mod> {
         .filter(|path| path.exists()) // filter out non-mod folders
         .filter_map(|path| std::fs::read_to_string(path).ok())
         .filter_map(|manifest_json| {
-            let manifest: Manifest = serde_json::from_str(&manifest_json).ok()?;
+            let manifest: Manifest = json5::from_str(&manifest_json).ok()?;
             let mut mod_struct: Mod = Mod::default();
             mod_struct.set_name(manifest.Name);
             mod_struct.set_version(manifest.Version);
-            mod_struct.set_min_api(manifest.MinimumApiVersion);
+            if let Some(s) = manifest.MinimumApiVersion {
+                mod_struct.set_min_api(s);
+            } else {
+                mod_struct.set_min_api(String::from("None"));
+            }
             mod_struct.set_enabled(true);
             Some(mod_struct)
         })
