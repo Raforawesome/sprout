2f8ac7d69123494077f042a9349099efe9b662b7
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-10-11 19:52:36 -0400

    Create `location_manager` module

--- src/interface.rs
+++ src/interface.rs
@@ -1,5 +1,6 @@
 //! Abstraction to act as an interface
 //! between code and the mods on the filesystem.
 
-pub mod mod_types;
+pub mod location_manager;
 pub mod mod_scanner;
+pub mod mod_types;
---
+++ src/interface/location_manager.rs
@@ -0,0 +1,48 @@
+//! # Location Manager
+//! A module to locate and maintain references to key locations
+//! for the program, such as enabled/disabled mod directories.
+
+// Dirs needed:
+// Enabled mods dir (inside game folder)
+// Disabled mods dir (probably in $XDG_CONFIG_HOME/.sprout/disabled)
+
+use std::path::{Path, PathBuf};
+use std::sync::LazyLock;
+
+pub fn sprout_home_dir<'a>() -> &'a Path {
+    static HOME_DIR: LazyLock<PathBuf> = LazyLock::new(|| {
+        let home: PathBuf = dirs::home_dir().expect("Failed to find home dir!");
+        let sprout_dir: PathBuf = home.join(".sprout");
+        if !sprout_dir.exists() {
+            std::fs::create_dir_all(&sprout_dir).expect("Failed to create sprout dir!");
+        }
+        sprout_dir
+    });
+    HOME_DIR.as_path()
+}
+
+pub fn disabled_mods_dir<'a>() -> &'a Path {
+    static DISABLED_DIR: LazyLock<PathBuf> = LazyLock::new(|| {
+        let disabled_dir: PathBuf = sprout_home_dir().join("disabled");
+        if !disabled_dir.exists() {
+            std::fs::create_dir_all(&disabled_dir).expect("Failed to create disabled dir!");
+        }
+        disabled_dir
+    });
+    DISABLED_DIR.as_path()
+}
+
+pub fn get_data_dir() -> PathBuf {
+    let mut path = dirs::data_dir().unwrap_or_else(|| {
+        eprintln!("Unsupported operating system.");
+        std::process::exit(1);
+    });
+    path.push(".sprout/");
+    if !path.exists() {
+        std::fs::create_dir(&path).unwrap_or_else(|_| {
+            eprintln!("No permission to write to data directory.");
+            std::process::exit(1);
+        });
+    };
+    path
+}
