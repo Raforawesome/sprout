dfa56b17f4a2c89d5a5b7218fd99147100820f77
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-11-02 15:14:45 -0400

    Add unit tests to `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,30 @@ use std::path::PathBuf;
 pub struct AppState {
     pub game_path: PathBuf,
 }
+
+#[allow(unused_imports)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_app_state() {
+        let app_state = AppState {
+            game_path: PathBuf::from("C:/Program Files/Stardew Valley"),
+        };
+
+        assert_eq!(app_state.game_path, PathBuf::from("C:/Program Files/Stardew Valley"));
+    }
+
+    #[test]
+    fn test_get_raw_mod_data() {
+        use crate::web::smapi_fetcher::get_raw_mod_data;
+        let result = get_raw_mod_data();
+        assert!(result.is_ok());
+        
+        let data = result.unwrap();
+        assert!(data.starts_with('['));
+        // assert!(data.ends_with(']'));
+        println!("{data}");
+    }
+}
