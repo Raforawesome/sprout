1495a631944c641c5dd90c1661c5b5ba2229f0c8
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-03-18 12:31:05 -0400

    Stash WIP update screen

--- src/views/update_mods.rs
+++ src/views/update_mods.rs
@@ -1,6 +1,6 @@
-use dioxus::prelude::*;
-use dioxus::document::Link;
 use crate::components::TitleHeader;
+use dioxus::document::Link;
+use dioxus::prelude::*;
 
 /// This component is spawned in a different window, so we treat this as a new root
 /// (include global.css again)
@@ -8,6 +8,12 @@ use crate::components::TitleHeader;
 pub fn UpdateScreen() -> Element {
     rsx! {
         Link { rel: "stylesheet", href: asset!("public/global.css") }
+        style { {include_str!("../css/mod_screen.css")} }
         TitleHeader { sub_title: "Update" }
+
+        // Mod table
+        div { // main grid
+            class: "mod-subgrid",
+        }
     }
 }
