70286435571974fed8388146f503da021996a551
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-03-20 17:49:41 -0400

    Add draw header toggle

--- src/components/title_header.rs
+++ src/components/title_header.rs
@@ -10,6 +10,8 @@ use crate::THEME;
 #[component]
 // #[cfg(not(target_os = "macos"))]
 pub fn TitleHeader(sub_title: String) -> Element {
+    let mut use_hr: Signal<bool> = use_signal(|| false);
+
     rsx! {
         Link { rel: "stylesheet", href: asset!("src/components/css/title_header.css") }
         if std::env::consts::OS != "macos" {
@@ -46,21 +48,33 @@ pub fn TitleHeader(sub_title: String) -> Element {
                 div {
                     class: "modal-box w-1/3 h-1/2 bg-base-200 flex flex-col items-center",
                     // box
-                    p { class: "text-base-content font-semibold text-lg", "Theme Selector" }
+                    p { class: "text-base-content font-semibold text-lg", "Sprout Settings" }
                     p { class: "text-base-content text-md", "Current theme: {THEME}" }
+
+                    div {
+                        class: "flex flex-row gap-3 my-4",
+                        input {
+                            type: "checkbox",
+                            checked: "{use_hr}",
+                            class: "checkbox",
+                            oninput: move |_| use_hr.set(!use_hr()),
+                        }
+                        p { class: "text-md", "Draw header rule" }
+                    }
                     details {
                         class: "dropdown",
                         summary {
-                            class: "btn btn-neutral mt-8 btn-wide",
+                            class: "btn btn-neutral mt-4 btn-wide",
                             "Show Themes"
                         }
                         ul {
                             class: "p-2 shadow menu dropdown-content rounded-box bg-base-100 w-52",
-                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "forest"), "forest" } }
                             li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "abyss"), "abyss" } }
-                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "cyberpunk"), "cyberpunk" } }
+                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "synthwave"), "synthwave" } }
                             li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "coffee"), "coffee" } }
                             li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "luxury"), "luxury" } }
+                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "forest"), "forest" } }
+                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "cyberpunk"), "cyberpunk" } }
                             li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "business"), "business" } }
                             li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "black"), "black" } }
                         }
@@ -79,7 +93,9 @@ pub fn TitleHeader(sub_title: String) -> Element {
             h1 { class: "text-5xl font-light text-base-content", "{sub_title}" }
         }
 
-        hr { class: "h-px border-accent my-1 opacity-50 drop-shadow-sm" }
+        if use_hr() {
+            hr { class: "h-px border-accent my-1 opacity-50 drop-shadow-sm" }
+        }
     }
 }
 
