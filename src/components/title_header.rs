463535503519f7a348639be582a7fd6026c195c4
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-03-19 20:48:03 -0400

    Only use titlebar icons on non-Mac platforms

--- src/components/title_header.rs
+++ src/components/title_header.rs
@@ -3,34 +3,61 @@ use dioxus::document::Link;
 use dioxus::prelude::*;
 
 #[component]
+// #[cfg(not(target_os = "macos"))]
 pub fn TitleHeader(sub_title: String) -> Element {
     rsx! {
         Link { rel: "stylesheet", href: asset!("src/components/css/title_header.css") }
-        div {
-            class: "title-buttons",
-            button {
-                class: "material-symbols-outlined button title-button",
-                style: "color:#ff9f43",
-                onclick: |_| window().set_minimized(true),
-                "close_fullscreen"
-            },
-            button {
-                class: "material-symbols-outlined button title-button",
-                style: "color:#ee5253",
-                onclick: |_| window().close(),
-                "cancel"
+        if std::env::consts::OS != "macos" {
+            div {
+                class: "title-buttons",
+                button {
+                    class: "material-symbols-outlined button title-button",
+                    style: "color:#ff9f43",
+                    onclick: |_| window().set_minimized(true),
+                    "close_fullscreen"
+                },
+                button {
+                    class: "material-symbols-outlined button title-button",
+                    style: "color:#ee5253",
+                    onclick: |_| window().close(),
+                    "cancel"
+                }
             }
         }
         div {
             onmousedown: |_| window().drag(),
             id: "title-header",
-            class: "title-header",
-            // img { class: "sprout-icon", src: asset!("public/sprout@1x-opt.webp") },
+            class: "w-full flex flex-row items-center justify-center gap-4 p-5",
+            button {
+                class: "mx-2",
+                img { class: "sprout-icon", src: asset!("public/sprout@1x-opt.webp") },
+            }
             h1 { class: "text-5xl font-bold text-base-content", "Sprout" },
             h1 { class: "text-5xl font-normal text-base-content", "|" },
             h1 { class: "text-5xl font-light text-base-content", "{sub_title}" }
         }
 
+        hr { class: "h-px border-accent my-1 opacity-50 drop-shadow-sm" }
         // hr { class: "divider" }
     }
 }
+
+// #[component]
+// #[cfg(target_os = "macos")]
+// pub fn TitleHeader(sub_title: String) -> Element {
+//     rsx! {
+//         Link { rel: "stylesheet", href: asset!("src/components/css/title_header.css") }
+//         div {
+//             onmousedown: |_| window().drag(),
+//             id: "title-header",
+//             class: "title-header",
+//             img { class: "sprout-icon", src: asset!("public/sprout@1x-opt.webp") },
+//             h1 { class: "text-5xl font-bold text-base-content", "Sprout" },
+//             h1 { class: "text-5xl font-normal text-base-content", "|" },
+//             h1 { class: "text-5xl font-light text-base-content", "{sub_title}" }
+//         }
+
+//         hr { class: "h-px border-accent my-1 opacity-50 drop-shadow-sm" }
+//         // hr { class: "divider" }
+//     }
+// }
