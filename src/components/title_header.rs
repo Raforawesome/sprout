1be23d1feffec8613307891f7a1c69bf7fa885d6
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-03-19 23:32:44 -0400

    Add theme switcher

--- src/components/title_header.rs
+++ src/components/title_header.rs
@@ -1,6 +1,11 @@
+use std::env::consts::OS;
+
 use dioxus::desktop::window;
 use dioxus::document::Link;
 use dioxus::prelude::*;
+use document::eval;
+
+use crate::THEME;
 
 #[component]
 // #[cfg(not(target_os = "macos"))]
@@ -27,37 +32,57 @@ pub fn TitleHeader(sub_title: String) -> Element {
         div {
             onmousedown: |_| window().drag(),
             id: "title-header",
-            class: "w-full flex flex-row items-center justify-center gap-4 p-5",
+            class: "w-full flex flex-row items-center justify-center gap-4 p-4",
+
             button {
                 class: "mx-2",
-                img { class: "sprout-icon", src: asset!("public/sprout@1x-opt.webp") },
+                onclick: prompt_theme_change,
+                img { class: "sprout-icon h-15", src: asset!("public/sprout@1x-opt.webp") },
             }
-            h1 { class: "text-5xl font-bold text-base-content", "Sprout" },
+            dialog {
+                id: "theme_modal",
+                class: "modal",
+
+                div {
+                    class: "modal-box w-1/3 h-1/2 bg-base-200 flex flex-col items-center",
+                    // box
+                    p { class: "text-base-content font-semibold text-lg", "Theme Selector" }
+                    p { class: "text-base-content text-md", "Current theme: {THEME}" }
+                    details {
+                        class: "dropdown",
+                        summary {
+                            class: "btn btn-neutral mt-8 btn-wide",
+                            "Show Themes"
+                        }
+                        ul {
+                            class: "p-2 shadow menu dropdown-content rounded-box bg-base-100 w-52",
+                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "forest"), "forest" } }
+                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "abyss"), "abyss" } }
+                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "cyberpunk"), "cyberpunk" } }
+                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "coffee"), "coffee" } }
+                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "luxury"), "luxury" } }
+                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "business"), "business" } }
+                            li { a { href: "#", onclick: move |_| THEME.with_mut(|s| *s = "black"), "black" } }
+                        }
+                    }
+                }
+
+                form {
+                    method: "dialog",
+                    class: "modal-backdrop",
+                    button { "close" }
+                }
+            }
+
+            h1 { class: "text-5xl font-semibold text-base-content", "Sprout" },
             h1 { class: "text-5xl font-normal text-base-content", "|" },
             h1 { class: "text-5xl font-light text-base-content", "{sub_title}" }
         }
 
         hr { class: "h-px border-accent my-1 opacity-50 drop-shadow-sm" }
-        // hr { class: "divider" }
     }
 }
 
-// #[component]
-// #[cfg(target_os = "macos")]
-// pub fn TitleHeader(sub_title: String) -> Element {
-//     rsx! {
-//         Link { rel: "stylesheet", href: asset!("src/components/css/title_header.css") }
-//         div {
-//             onmousedown: |_| window().drag(),
-//             id: "title-header",
-//             class: "title-header",
-//             img { class: "sprout-icon", src: asset!("public/sprout@1x-opt.webp") },
-//             h1 { class: "text-5xl font-bold text-base-content", "Sprout" },
-//             h1 { class: "text-5xl font-normal text-base-content", "|" },
-//             h1 { class: "text-5xl font-light text-base-content", "{sub_title}" }
-//         }
-
-//         hr { class: "h-px border-accent my-1 opacity-50 drop-shadow-sm" }
-//         // hr { class: "divider" }
-//     }
-// }
+fn prompt_theme_change(_: Event<MouseData>) {
+    eval("document.getElementById('theme_modal').showModal()");
+}
