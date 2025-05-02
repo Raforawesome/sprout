792d946f8068a1c84f6c5f7908b6a64a203e16fd
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-05-02 16:23:15 -0400

    Add raw github mod data decoder

---
+++ src/libsprout/smapi/gh_decoder.rs
@@ -0,0 +1,54 @@
+use dioxus::logger::tracing::warn;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct GhModData {
+    #[serde(rename = "$schema")]
+    schema: String,
+    mods: Vec<ModListing>,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct ModListing {
+    name: String,
+    author: String,
+    id: String,
+    nexus: Option<u32>,
+    curse: Option<u32>,
+    moddrop: Option<u32>,
+    github: Option<String>,
+    broke_in: Option<String>,
+    summary: Option<String>,
+    unofficial_update: Option<UnofficialUpdate>,
+}
+
+impl ModListing {
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn author(&self) -> &str {
+        &self.author
+    }
+
+    pub fn id(&self) -> &str {
+        &self.id
+    }
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct UnofficialUpdate {
+    version: String,
+    url: String,
+}
+
+pub fn parse_gh_data(data: &str) -> Vec<ModListing> {
+    let Ok(data) = json5::from_str::<GhModData>(data) else {
+        println!("failed parsing");
+        warn!("Failed to parse github mod data.");
+        return Vec::new();
+    };
+    data.mods
+}
